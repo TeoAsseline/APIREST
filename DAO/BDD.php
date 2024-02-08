e9cb9d4cd92479e82d8b5b9819206582ca62294e
Téo Asseline <asselineteo@gmail.com>
2024-02-08 18:53:58 +0100

    changement bdd

--- DAO/BDD.php
+++ DAO/BDD.php
@@ -4,10 +4,10 @@
 //----------------------------------------------------------------------//
     function ConnexionBDD(){
         ///Connexion au serveur MySQL avec PDO
-        $server = '145.14.156.192';
-        $login = 'u563109936_teoarthur';
-        $mdp = '$B5zN1gN52r';
-        $db = 'u563109936_APIREST_articl';
+        $server = 'localhost';
+        $login = 'root';
+        $mdp = '';
+        $db = 'apirest';
         try {
             $linkpdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", $login, $mdp);
             $linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
